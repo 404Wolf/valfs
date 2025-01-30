63e38a5b59e6d71e1c52c1f287f34cbe82654b6a
Wolf Mermelstein <wolf@404wolf.com>
2025-01-30 14:21:17 -0500

    increase wait

--- fuse/valfs/myblobs/myblobs_test.go
+++ fuse/valfs/myblobs/myblobs_test.go
@@ -24,7 +24,7 @@ func generateRandomFileName(prefix string) string {
 }
 
 func sleepAfterWrite() {
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(1 * time.Second)
 }
 
 func TestCreateBlobs(t *testing.T) {
