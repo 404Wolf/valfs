b20e24a8bebe4f4add8d58efde94243b32aab10c
Wolf Mermelstein <wolf@404wolf.com>
2025-02-04 00:03:04 -0500

    fix nix build

--- package.nix
+++ package.nix
@@ -1,6 +1,7 @@
 {buildGoModule, ...}:
 buildGoModule {
   name = "valfs";
-  vendorHash = "sha256-KP43QJ/GmvTic+Yv3LLCCO+yBDmdaWpgzGcnYq8VzYw=";
+  vendorHash = "sha256-wJ2k99i6yXUT+LV6AKLPaJke4xv/dZt47oKPGeQyWoU=";
+  doCheck = false;
   src = ./.;
 }
